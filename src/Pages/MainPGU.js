bf10351c46e709538d9ea383c494f31203ae6c8e
Dimitris Dimochronis <dimitris.dimochronis@gmail.com>
2025-01-03 21:45:35 +0200

    Start of main search bar

--- src/Pages/MainPGU.js
+++ src/Pages/MainPGU.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Accordion from 'react-bootstrap/Accordion';
 import JobofferReview from "../Components/EpaggelmatiesComponent/JobofferReview";
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
 function MainPGU(props) {
   const navigate = useNavigate();
+
+  const [age, setAge] = useState("");
+  const [area, setArea] = useState("");
+
+  const areasOfGreece = [
+    "Αθήνα",
+    "Θεσσαλονίκη",
+    "Πάτρα",
+    "Ηράκλειο",
+    "Λάρισα",
+    "Βόλος",
+    "Ιωάννινα",
+    "Καβάλα",
+    "Χανιά",
+    "Ρόδος",
+  ];
+
+  const ages = [
+    "0.5",
+    "1",
+    "1.5",
+    "2",
+    "2.5",
+  ];
+
+  const education = [
+    "Δημοτικό",
+    "Γυμνάσιο",
+    "Λύκειο",
+    "Πανεπιστήμιο",
+  ];
 
   const handleSearchRedirect = () => {
     navigate('/anazitisi');
@@ -26,29 +58,54 @@ function MainPGU(props) {
         </div>
 
         <div style={{ backgroundColor: "white", position: "relative", zIndex: 2, display: "flex", width: "50%", justifyContent: "center", margin: "0 auto",
-                     flexDirection: "column", border: "1px solid black", borderRadius: "15px", height: "20vh", marginTop: "-10vh" }}>
+                     flexDirection: "row", border: "1px solid black", borderRadius: "15px", height: "20vh", marginTop: "-10vh" }}>
 
-          <h6 style={{ textAlign: "center" }}>Βρείτε τον/την επαγγελματία που σας ταιριάζει!</h6>
+          <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "30%" }}>
+            <h6> Επιλογή Περιοχής </h6>
+            <FormControl fullWidth style={{ marginTop: "1vh", fontSize:"5%" }}>
+              <InputLabel id="area-select-label"></InputLabel>
+              <Select
+                labelId="area-select-label"
+                value={area}
+                onChange={(e) => setArea(e.target.value)}
+                style={{ width: "80%" }}
+                displayEmpty
+              >
+                {<MenuItem value="">Επιλέξτε Περιοχή</MenuItem>}
+                {areasOfGreece.map((area) => (
+                  <MenuItem key={area} value={area.toLowerCase()}>
+                    {area}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
 
-          <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", marginTop: "3%" }}>
+          <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "30%" }}>
+            <h6> Επιλογή Ηλικίας </h6>
+            <FormControl fullWidth style={{ marginTop: "1vh" }}>
+              <InputLabel id="age-select-label"></InputLabel>
+              <Select
+                labelId="age-select-label"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+                style={{ width: "80%" }}
+                displayEmpty
+              >
+                {<MenuItem value="">Επιλέξτε Ηλικία</MenuItem>}
+                {ages.map((item) => (
+                  <MenuItem key={item} value={item.toLowerCase()}>
+                    {item} ετών
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
 
-            <div style={{ display: "flex", width: "30%", outline: "1px solid black", marginLeft: "10%", borderRadius: "15px", height: "5vh" }}>
-
-              <Link to="/anazitisi" className="nav-link" style={{ background: "none", border: "none", padding: 0, cursor: "pointer", width: "100%", display: "flex", 
-                                          alignItems: "center", justifyContent: "space-between", textDecoration: "none" }} onClick={handleSearchRedirect} >
-                <span style={{ fontWeight: 100, marginLeft: "3%"}}>
-                  Βρείτε αυτό που ψάχνετε
-                </span>
-
-                <img src="/search (1).svg" alt="Search" style={{ width: "24px", height: "24px", marginRight: "3%" }} />
-              </Link>
-
-            </div>
-
-            <Link to="/epaggelmaties" className="nav-link" style={{background: "none",border: "none",padding: 0,cursor: "pointer",width: "30%",display: "flex",alignItems: "center",justifyContent: "space-between", textDecoration: "none" }}>
-              <span style={{ marginRight: "10%", textDecoration: "underline" }}>Βρείτε εργασία</span>
-            </Link>
-            
+          <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "30%" }}>
+            <button style={{ backgroundColor: "#2b8cbe", color: "white", border: "none", borderRadius: "5px", width: "60%", height: "30%", marginTop: "2vh" }}>
+              <h6 style={{ marginLeft: "10px", marginTop: "1vh", textDecoration:"underline" }}>Αναζήτηση</h6>
+            </button>
           </div>
 
         </div>
