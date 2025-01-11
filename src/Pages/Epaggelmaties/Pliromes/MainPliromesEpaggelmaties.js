ad2dd268cfc4dafe6859012468a39c90eb4b7e05
Ragkosgiorgos <100347566+Ragkosgiorgos@users.noreply.github.com>
2025-01-11 17:39:13 +0200

    minor changes

--- src/Pages/Epaggelmaties/Pliromes/MainPliromesEpaggelmaties.js
+++ src/Pages/Epaggelmaties/Pliromes/MainPliromesEpaggelmaties.js
@@ -2,102 +2,159 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import { collection, query, where, getDocs } from 'firebase/firestore';
-import { FIREBASE_AUTH,FIREBASE_DB } from '../../../config/firebase';
+import { FIREBASE_AUTH, FIREBASE_DB } from '../../../config/firebase';
 import Header from '../../../Components/Header';
 import Footer from '../../../Components/Footer';
 import Loader from '../../../Components/Loader';
 
+function MainPliromesEpaggelmaties() {
+  const navigate = useNavigate();
 
-function MainPliromesEpaggelmaties(){
-    const navigate = useNavigate();
+  // Check if user is logged in, get the user's UUID and fetch user data
+  const [uuid, setUuid] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [parents, setParents] = useState([]);
+  const [contracts, setContracts] = useState([]);
 
-    // Check if user is logged in, get the user's UUID and fetch user data
-    const [uuid, setUuid] = useState(null);
-    const [loading, setLoading] = useState(false);
-  
-    useEffect(() => {
-      const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
-        if (user) {
-          setUuid(user.uid);
-        }
-      });
-      return () => unsubscribe();
-    }, []);
-  
-    // Fetch the job posts' data
-    const [contracts, setContracts] = useState([]);
-  
-    useEffect(() => {
-      if (uuid) {
-        const fetchPosts = async () => {
-          try {
-            setLoading(true);
-            const q = query(collection(FIREBASE_DB, 'payments'), where('id_b', '==', uuid));
-            const querySnapshot = await getDocs(q);
-            const posts = querySnapshot.docs.map((doc) => ({
-              id: doc.id,
-              ...doc.data(),
-            }));
-            setContracts(posts);
-          } catch (error) {
-            console.error('Error fetching posts:', error);
-          } finally {
-            setLoading(false);
-          }
-        };
-        fetchPosts(); // Fetch posts when component mounts
+  // Check user authentication
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      if (user) {
+        setUuid(user.uid);
       }
-    }, [uuid]); // Runs when UUID changes
-  
-    if (loading) {
-      return <Loader />;
+    });
+    return () => unsubscribe();
+  }, []);
+
+  // Fetch parent users
+  useEffect(() => {
+    const fetchParents = async () => {
+      try {
+        setLoading(true);
+        const parentsRef = collection(FIREBASE_DB, 'user');
+        const q = query(parentsRef, where('property', '==', 'parent'));
+        const querySnapshot = await getDocs(q);
+        const fetchedParents = querySnapshot.docs.map((doc) => ({
+          userId: doc.id,
+          ...doc.data(),
+        }));
+        setParents(fetchedParents);
+      } catch (error) {
+        console.error('Error fetching parents:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchParents();
+  }, []);
+
+  // Fetch the job posts' data
+  useEffect(() => {
+    if (uuid) {
+      const fetchPosts = async () => {
+        try {
+          setLoading(true);
+          const q = query(collection(FIREBASE_DB, 'payments'), where('id_b', '==', uuid));
+          const querySnapshot = await getDocs(q);
+          const posts = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          setContracts(posts);
+        } catch (error) {
+          console.error('Error fetching posts:', error);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchPosts();
     }
-  
-    return (
-      <div style={{ justifyContent: 'space-between', display: 'flex', flexDirection: 'column', overflow: 'auto', minHeight: '100vh' }}>
-        <div>
-          <Header />
-  
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <div style={{ flex: 1 }}>
-              {"breadcrambs"}
-  
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '2%' }}>
-                  <h2 style={{ fontWeight: 'bold', textAlign: 'center', marginTop: '3%' }}>Οι πληρωμές μου</h2>
-              </div>
-  
-  
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '2%' }}>
-                <table style={{ width: '80%', backgroundColor: '#D9EAFD', textAlign: 'center', borderRadius: '10px' }}>
-                  <thead style={{ lineHeight: '2em' }}>
-                    <tr style={{ borderBottom: '2px solid #333' }}>
-                      <th>Κωδικός συμβολαίου</th>
-                      <th>Ονοματεπώνυμο Γονέα</th>
-                      <th>Κωδίκος Voucher</th>
-                      <th style={{ width: '150px' }}></th>
-                      <th></th>
+  }, [uuid]);
+
+  const findParentName = (parentId) => {
+    const parent = parents.find((parent) => parent.userId === parentId);
+    return parent ? `${parent.firstName} ${parent.lastName}` : 'Άγνωστο';
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <div
+      style={{
+        justifyContent: 'space-between',
+        display: 'flex',
+        flexDirection: 'column',
+        overflow: 'auto',
+        minHeight: '100vh',
+      }}
+    >
+      <div>
+        <Header />
+
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <div style={{ flex: 1 }}>
+            {"breadcrambs"}
+
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop: '2%',
+              }}
+            >
+              <h2 style={{ fontWeight: 'bold', textAlign: 'center', marginTop: '3%' }}>
+                Οι πληρωμές μου
+              </h2>
+            </div>
+
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop: '2%',
+              }}
+            >
+              <table
+                style={{
+                  width: '80%',
+                  backgroundColor: '#D9EAFD',
+                  textAlign: 'center',
+                  borderRadius: '10px',
+                }}
+              >
+                <thead style={{ lineHeight: '2em' }}>
+                  <tr style={{ borderBottom: '2px solid #333' }}>
+                    <th>Κωδικός συμβολαίου</th>
+                    <th>Ονοματεπώνυμο Γονέα</th>
+                    <th>Κωδίκος Voucher</th>
+                    <th style={{ width: '150px' }}></th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {contracts.map((contract) => (
+                    <tr key={contract.id}>
+                      <td>{contract.id}</td>
+                      <td>{findParentName(contract.id_p)}</td>
+                      <td>{contract.id_c}</td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {contracts.map((contract) => (
-                      <tr key={contract.id}>
-                        <td>{contract.id}</td>
-                        <td>{/* Fill this with appropriate data */}</td>
-                        <td>{contract.id_c}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-  
-        <Footer />
       </div>
-    );
-  }
-  
 
+      <Footer />
+    </div>
+  );
+}
 
 export default MainPliromesEpaggelmaties;
